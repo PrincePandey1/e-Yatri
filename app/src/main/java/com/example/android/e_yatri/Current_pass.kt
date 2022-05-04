ebd30afa0e85529332011ed1e714905b8e73a2e5
Prince Pandey <princekupandey786@gmail.com>
2022-05-05 00:00:45 +0530

    Added Activities

---
+++ app/src/main/java/com/example/android/e_yatri/Current_pass.kt
@@ -0,0 +1,11 @@
+package com.example.android.e_yatri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Current_pass : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_current_pass)
+    }
+}
