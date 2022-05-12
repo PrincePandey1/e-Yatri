85cf0af886b26b7104114c5f958f2df65fae8f40
Prince Pandey <princekupandey786@gmail.com>
2022-05-12 23:21:06 +0530

    UI Designing

---
+++ app/src/main/java/com/example/android/e_yatri/Select_Payment_method.kt
@@ -0,0 +1,11 @@
+package com.example.android.e_yatri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Select_Payment_method : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select__payment_method)
+    }
+}
